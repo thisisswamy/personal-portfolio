112b01d521ed939a79e946aa087202c113490bb6
thisisswamy <thudumswamy786@gmail.com>
2023-11-12 17:03:33 +0530

    added the work and projects

--- src/app/common/components/project-card/project-card.component.ts
+++ src/app/common/components/project-card/project-card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-project-card',
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./project-card.component.scss']
 })
 export class ProjectCardComponent {
+  @Input() projectDetails:any;
 
 }
---
+++ src/app/common/service/DataConstants.ts
@@ -0,0 +1,182 @@
+export class DataConstants {
+  public static skillsDetails = [
+    {
+      name: 'Frontend Technologies',
+      technolgies: [
+        {
+          techName: 'HTML5',
+          proficiency: 95,
+        },
+        {
+          techName: 'CSS',
+          proficiency: 80,
+        },
+        {
+          techName: 'Java Script',
+          proficiency: 75,
+        },
+        {
+          techName: 'Type Script',
+          proficiency: 85,
+        },
+        {
+          techName: 'Angular',
+          proficiency: 90,
+        },
+      ],
+    },
+    {
+      name: 'Database & Version Control',
+      technolgies: [
+        {
+          techName: 'SQL',
+          proficiency: 80,
+        },
+        {
+          techName: 'MySQL',
+          proficiency: 70,
+        },
+        {
+          techName: 'GIT',
+          proficiency: 85,
+        },
+        {
+          techName: 'GitHub',
+          proficiency: 85,
+        },
+        {
+          techName: 'Bit Bucket',
+          proficiency: 90,
+        },
+      ],
+    },
+    {
+      name: 'Backend Technologies',
+      technolgies: [
+        {
+          techName: 'Java',
+          proficiency: 85,
+        },
+        {
+          techName: 'Spring Boot',
+          proficiency: 85,
+        },
+        {
+          techName: 'Spring Framework',
+          proficiency: 80,
+        },
+        {
+          techName: 'Microservices',
+          proficiency: 70,
+        },
+      ],
+    },
+  ];
+
+  public static projectDetails = [
+    {
+      company: 'Tata Consultency Services',
+      designation: 'System Engineer',
+      role: 'Frontend developer',
+      technology: 'Angular',
+      fromToDates: '2021 - Present',
+      projects: [
+        {
+          title: 'API Developer Portal and Admin Portal.',
+          responsibilities: [
+            'Have developed clean, responsive and user friendly user interfaces.',
+            "Integrated multiple API's with frontend.",
+            'Implemented the state management using NgRx throught the applications.',
+            'Worked with angular material for admin portal.',
+            'Have worked and developed the Jasmine unit test cases.',
+            'Handled Error Scenerios all well with appropreiate messageson UI',
+          ],
+          keywords: [
+            'Angular',
+            'NgRx',
+            'RxJs',
+            'Rest API',
+            'Postman',
+            'Authentication',
+            'State Management',
+            'Unit testing',
+            'Jasmine',
+            'NginX',
+          ],
+        },
+        {
+          title: 'Proxy Generated Tool.',
+          responsibilities: [
+            'Have developed clean, responsive, and conditional rendering user interfaces.',
+            "Integrated MultiPart API's with frontend.",
+            "Have handled free data from users for Rest API's.",
+          ],
+          keywords: [
+            'Angular',
+            'RxJs',
+            'Rest API',
+            'Postman',
+            'Unit testing',
+            'Jasmine',
+            'NginX',
+          ],
+        },
+      ],
+    },
+    {
+      company: 'for Personal Career Growth',
+      designation: 'Java & SpringBoot',
+      role: 'Backend developer',
+      technology: 'Java',
+      fromToDates: '2021 - Present',
+      projects: [
+        {
+          title: 'Moview Review Application',
+          responsibilities: [
+            'Have developed REST APIs for Microservices Architecture based application.',
+            'Integrated Spring Cloud Gateway and Spring Security.',
+            'Documented with swagger API.',
+            'Worked with JWT Tokens for AUthentication and Authorization.',
+            'Have worked and developed with Mongo DB and MySQL Databases for Data handling.',
+            'Developed API for all User scenarios - Register,Login,Forgot Password, Account Verification Link and Reset Password',
+            'Handled Error Scenerios all well with appropreiate responses.',
+          ],
+          keywords: [
+            'Java',
+            'Spring Framework',
+            'Spring Boot',
+            'Spring Security',
+            'API Gateway',
+            'Discovery Service',
+            'Microservices',
+            'Postman',
+            'JWT Token',
+          ],
+        },
+        {
+          title: 'Notes Application.',
+          responsibilities: [
+            'Have developed REST APIs for Microservices Architecture based application.',
+            'Integrated Spring Cloud Gateway and Spring Security.',
+            'Documented with swagger API.',
+            'Worked with JWT Tokens for AUthentication and Authorization.',
+            'Have worked and developed with Mongo DB and MySQL Databases for Data handling.',
+            'Developed API for all User scenarios - Register,Login,Forgot Password, Account Verification Link and Reset Password',
+            'Handled Error Scenerios all well with appropreiate responses.',
+          ],
+          keywords: [
+            'Java',
+            'Spring Framework',
+            'Spring Boot',
+            'Spring Security',
+            'API Gateway',
+            'Discovery Service',
+            'Microservices',
+            'Postman',
+            'JWT Token',
+          ],
+        },
+      ],
+    },
+  ];
+}
